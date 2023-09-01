50c0a39ab4133ffa3dee38150c005bc1be15cfdb
SaifAlam <kaif0350@gmail.com>
2023-09-01 16:06:48 +0530

    ReactRouter Done

---
+++ Dashboard_ecommerce/index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const cors = require('cors');
+require('./DB/config');
+const User = require('./DB/Users');
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+
+app.post("/register", async (req, res) => {
+    let user = new User(req.body);
+    let result = await user.save();
+    result = result.toObject();
+    delete result.password
+    res.send(result);
+})
+
+app.post("/login", async (req, res) => {
+    console.log(req.body)
+    if (req.body.password && req.body.email) {
+        let user = await User.findOne(req.body).select('-password');
+        if (user) {
+            res.send(user)
+        } else {
+            res.send({ result: "No User Found" })
+        }
+    }
+    else {
+        res.send({ result: "No User Found" })
+    }
+
+
+})
+
+
+app.listen(5000);
